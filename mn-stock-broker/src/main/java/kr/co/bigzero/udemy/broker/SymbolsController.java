6f4588f5c7d243eb638baf7218e884ea3c638beb
bigzero <bigzero389@gmail.com>
2023-12-15 19:14:18 +0900

    end: chapter23 in section3

--- mn-stock-broker/src/main/java/kr/co/bigzero/udemy/broker/SymbolsController.java
+++ mn-stock-broker/src/main/java/kr/co/bigzero/udemy/broker/SymbolsController.java
@@ -2,10 +2,13 @@ package kr.co.bigzero.udemy.broker;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
 import kr.co.bigzero.udemy.broker.data.InMemoryStore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller("/symbols")
 public class SymbolsController {
@@ -20,4 +23,18 @@ public class SymbolsController {
   public List<Symbol> getAll() {
     return new ArrayList<>(inMemoryStore.getSymbols().values());
   }
+
+  @Get("{value}")
+  public Symbol getSymbolByValue(@PathVariable String value) {
+    return inMemoryStore.getSymbols().get(value);
+  }
+
+  @Get("/filter{?max,offset}")
+  public List<Symbol> getSymbols(@QueryValue Optional<Integer> max, @QueryValue Optional<Integer> offset) {
+    return inMemoryStore.getSymbols().values()
+        .stream().skip(offset.orElse(0))
+        .limit(max.orElse(10))
+        .toList();
+  }
 }
+
---
+++ mn-stock-broker/src/test/java/kr/co/bigzero/udemy/SymbolsControllerTest.java
@@ -0,0 +1,73 @@
+package kr.co.bigzero.udemy;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import kr.co.bigzero.udemy.broker.Symbol;
+import kr.co.bigzero.udemy.broker.data.InMemoryStore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@MicronautTest
+public class SymbolsControllerTest {
+
+  private final Logger LOG = LoggerFactory.getLogger(SymbolsControllerTest.class);
+
+  @Inject
+  @Client("/symbols")
+  HttpClient client;
+
+  @Inject
+  InMemoryStore inMemoryStore;
+
+  @BeforeEach
+  void setup() {
+    inMemoryStore.initializeWith(10);
+  }
+
+  @Test
+  void symbolsEndpointReturnsListOfSymbol() {
+    var response = client.toBlocking().exchange("/", JsonNode.class);
+    assertEquals(HttpStatus.OK, response.getStatus());
+    assertEquals(10, response.getBody().get().size());
+  }
+
+  @Test
+  void symbolsEndpointReturnsTheCorrectSymbol() {
+    var testSymbol = new Symbol("TEST");
+    inMemoryStore.getSymbols().put(testSymbol.value(), testSymbol);
+    var response = client.toBlocking().exchange("/" + testSymbol.value(), Symbol.class);
+    assertEquals(HttpStatus.OK, response.getStatus());
+    assertEquals(testSymbol, response.getBody().get());
+  }
+
+  @Test
+  void symbolsEndpointReturnsListOfSymbolTakingQueryParametersIntoAccount() {
+    var max10 = client.toBlocking().exchange("/filter?max=10", JsonNode.class);
+    assertEquals(HttpStatus.OK, max10.getStatus());
+    LOG.debug("Max: 10: {}", max10.getBody().get().toPrettyString());
+    assertEquals(10, max10.getBody().get().size());
+
+    var offset7 = client.toBlocking().exchange("/filter?offset=7", JsonNode.class);
+    assertEquals(HttpStatus.OK, offset7.getStatus());
+    LOG.debug("Offset: 7: {}", offset7.getBody().get().toPrettyString());
+    assertEquals(3, offset7.getBody().get().size());
+
+    var max2Offset7 = client.toBlocking().exchange("/filter?max=2&offset=7", JsonNode.class);
+    assertEquals(HttpStatus.OK, max2Offset7.getStatus());
+    LOG.debug("Max 2, Offset: 7: {}", max2Offset7.getBody().get().toPrettyString());
+    assertEquals(2, max2Offset7.getBody().get().size());
+  }
+
+}
+
+
+
